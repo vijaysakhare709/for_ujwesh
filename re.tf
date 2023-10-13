ad6f6da69358c2a24cd7e2593da33adac448cd6f
vijaysakhare709 <129182134+vijaysakhare709@users.noreply.github.com>
2023-10-13 17:46:46 +0530

    Create re.tf

---
+++ re.tf
@@ -0,0 +1,31 @@
+Dear Paramount Health Insurance,
+
+I hope this message finds you well. I am writing to request reimbursement for medical expenses incurred under my health insurance policy.
+
+Intimation No.- Date	3946174 - 06/10/2023
+Corporate	RELX INDIA PRIVATE LIMITED
+Policy No	2-81-23-0000232-000
+Employee Name	VIJAY SAKHARE
+Employee Code	00000645040
+Name of the Claimant	VIJAY SAKHARE
+Relation of the claimant with employee	EMPLOYEE
+PHS ID	39183934
+Claim Type	REIMBURSEMENT
+Ailment	
+Name of Hospital	BHAGAT MULTISPECIALITY HOSPITAL
+Download Claim Form	Click here to download Reimbursement Claim Form 
+
+	
+Amount Incurred:  6368 -/ Rs
+
+I have attached all necessary documentation to support my reimbursement claim, including copies of medical bills, invoices, and receipts related to the aforementioned expenses. These expenses were incurred due to a genuine medical condition, and I am eligible for coverage according to the terms of my health insurance policy.
+
+I kindly request that you process my reimbursement claim at your earliest convenience and in accordance with the terms and conditions outlined in my insurance policy. As per the policy requirements, I have completed the reimbursement claim form and attached it for your reference.
+
+If there are any further documents or information needed to facilitate this reimbursement claim, please do not hesitate to contact me.
+
+Thank you for your assistance in this matter.
+
+Sincerely,
+
+Vijay Sakhare
